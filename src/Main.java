b5bd013d9575ea5afc6d2a023967a6cd9e8523c8
sboreczek <szymih2@gmail.com>
2022-01-17 13:35:05 +0100

    ćwiczenie 1 zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,78 @@
+import jdk.jshell.Snippet;
+
+import java.util.Scanner;
+
 public class Main {
+
+
+
 
     public static void main(String[] args) {
 
-        System.out.println("Witaj świecie!");
+        Scanner skan = new Scanner(System.in);
+        int x;
+
+        while(true){
+        x = skan.nextInt();
+        if(x == 0) {
+            System.out.println(StatusEnum.KONIEC);
+            break;
+        }
+        else {
+            System.out.println(StatusEnum.KONTYNUUJEMY);
+            switch (x) {
+                case 1:
+                    System.out.println(LiczbyEnum.JEDEN);
+                    break;
+                case 2:
+                    System.out.println(LiczbyEnum.DWA);
+                    break;
+                case 3:
+                    System.out.println(LiczbyEnum.TRZY);
+                    break;
+                case 4:
+                    System.out.println(LiczbyEnum.CZTERY);
+                    break;
+                case 5:
+                    System.out.println(LiczbyEnum.PIEC);
+                    break;
+                case 6:
+                    System.out.println(LiczbyEnum.SZESC);
+                    break;
+
+            }
+        }
+        }
+        /* ćw 1 enum-y */
+
+        /* zad.1
+        a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
+        b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
+        c) stworzyć pętlę while z wartością true w warunku (nieskończona pętla),
+        d) niech w pętli są podawane wartości liczbowe z klawiatury,
+        e) jeśli użytkownik poda z klawiatury 0 to program ma kończyć działanie,
+        f) umieścić w pętli switch() case: które będą wypisywały cyfry słownie odpowiednio
+        do podanych cyfr przez użytkownika,
+        g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
+        */
+
+        /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
+         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
+
 
     }
+
+    enum LiczbyEnum {
+
+        JEDEN,DWA,TRZY,CZTERY,PIEC,SZESC;
+
+    }
+
+    enum StatusEnum{
+
+        KONTYNUUJEMY,KONIEC;
+
+    }
+
+
 }
