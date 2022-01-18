1ec6eb4d42dc2e972bb76a9a585ca5cc6c53308e
sboreczek <szymih2@gmail.com>
2022-01-18 12:55:48 +0100

    ćwiczenie 2 zrobione

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,51 @@
 import jdk.jshell.Snippet;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
 
 
-
-
     public static void main(String[] args) {
 
-        Scanner skan = new Scanner(System.in);
-        int x;
+        List<Student> studentlist = new ArrayList<>();
 
-        while(true){
-        x = skan.nextInt();
-        if(x == 0) {
-            System.out.println(StatusEnum.KONIEC);
-            break;
-        }
-        else {
-            System.out.println(StatusEnum.KONTYNUUJEMY);
-            switch (x) {
-                case 1:
-                    System.out.println(LiczbyEnum.JEDEN);
-                    break;
-                case 2:
-                    System.out.println(LiczbyEnum.DWA);
-                    break;
-                case 3:
-                    System.out.println(LiczbyEnum.TRZY);
-                    break;
-                case 4:
-                    System.out.println(LiczbyEnum.CZTERY);
-                    break;
-                case 5:
-                    System.out.println(LiczbyEnum.PIEC);
-                    break;
-                case 6:
-                    System.out.println(LiczbyEnum.SZESC);
-                    break;
+        Student student = new Student();
+        Student student1 = new Student();
+        Student student2 = new Student();
+        student.imie = "Andrzej";
+        student.nazwisko = "Zygmuntowski";
+        student.liczba = 24;
 
-            }
-        }
-        }
-        /* ćw 1 enum-y */
+        student1.imie = "Tomasz";
+        student1.nazwisko = "Karolak";
+        student1.liczba = 43;
+        student1.prawda = false;
 
+        student2.imie = "Maciej";
+        student2.nazwisko = "Kawulski";
+        student2.liczba = 15;
+
+        studentlist.add(student);
+        studentlist.add(student1);
+        studentlist.add(student2);
+        for (Student s : studentlist){
+
+            System.out.println(s.imie + "\n" + s.nazwisko + "\n" + s.liczba + "\n" + s.prawda + "\n" );
+
+        }
+
+
+        }
+        /* ćw 2 klasy */
         /* zad.1
-        a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
-        b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
-        c) stworzyć pętlę while z wartością true w warunku (nieskończona pętla),
-        d) niech w pętli są podawane wartości liczbowe z klawiatury,
-        e) jeśli użytkownik poda z klawiatury 0 to program ma kończyć działanie,
-        f) umieścić w pętli switch() case: które będą wypisywały cyfry słownie odpowiednio
-        do podanych cyfr przez użytkownika,
-        g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
-        */
-
-        /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
-         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
-
-
-    }
-
-    enum LiczbyEnum {
-
-        JEDEN,DWA,TRZY,CZTERY,PIEC,SZESC;
-
-    }
-
-    enum StatusEnum{
-
-        KONTYNUUJEMY,KONIEC;
-
-    }
-
+            a) Stworzyć publiczną klasę Student z 2-ma polami String, 1 polem int i 1 polem boolean,
+            klasa ma się znajdować w osobnym pliku Student.java
+            b) stworzyć 3 objekty klasy student i wstawić je do tablicy
+            c) wyświetlić dane z tablicy, użyć pętli for lub forEach
+            d) dane powinny być estetycznie przedstawione
+         */
 
 }
---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    String imie,nazwisko;
+    int liczba;
+    boolean prawda = true;
+
+}
+
+
+
+
+/* ćw 2 klasy */
+        /* zad.1
+            a) Stworzyć publiczną klasę Student z 2-ma polami String, 1 polem int i 1 polem boolean,
+            klasa ma się znajdować w osobnym pliku Student.java
+            b) stworzyć 3 objekty klasy student i wstawić je do tablicy
+            c) wyświetlić dane z tablicy, użyć pętli for lub forEach
+            d) dane powinny być estetycznie przedstawione
+         */
