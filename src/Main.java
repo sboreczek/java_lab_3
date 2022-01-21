f237e5a587efe681e8457ade608288881e8af964
sboreczek <szymih2@gmail.com>
2022-01-21 10:48:47 +0100

    Wyczyszczenie

--- src/Main.java
+++ src/Main.java
@@ -9,70 +9,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner skan = new Scanner(System.in);
-        int x;
 
-        while(true){
-        x = skan.nextInt();
-        if(x == 0) {
-            System.out.println(StatusEnum.KONIEC);
-            break;
-        }
-        else {
-            System.out.println(StatusEnum.KONTYNUUJEMY);
-            switch (x) {
-                case 1:
-                    System.out.println(LiczbyEnum.JEDEN);
-                    break;
-                case 2:
-                    System.out.println(LiczbyEnum.DWA);
-                    break;
-                case 3:
-                    System.out.println(LiczbyEnum.TRZY);
-                    break;
-                case 4:
-                    System.out.println(LiczbyEnum.CZTERY);
-                    break;
-                case 5:
-                    System.out.println(LiczbyEnum.PIEC);
-                    break;
-                case 6:
-                    System.out.println(LiczbyEnum.SZESC);
-                    break;
 
-            }
-        }
-        }
-        /* ćw 1 enum-y */
+
+    }
+}
+
+/* ćw 4 klasy */
 
         /* zad.1
-        a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
-        b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
-        c) stworzyć pętlę while z wartością true w warunku (nieskończona pętla),
-        d) niech w pętli są podawane wartości liczbowe z klawiatury,
-        e) jeśli użytkownik poda z klawiatury 0 to program ma kończyć działanie,
-        f) umieścić w pętli switch() case: które będą wypisywały cyfry słownie odpowiednio
-        do podanych cyfr przez użytkownika,
-        g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
-        */
+            a) Stworzyć klasę Osoba z polami prywatnymi:
+            - imie (String),
+            - nazwisko (String),
+            - indeks (Integer)
+            b) stworzyc konstruktory, gettery i settery
+            c) napisać metodę w klasie Osoba która zwróci informację o osobie,
+         */
 
-        /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
-         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
+        /* zad.2
+            a) Stworzyć klasę Student z polami prywatnymi
+            - Osoba osoba,
+            - WydzialEnum wydzial;
+            b) stworzyć enum WydzialEnum (w nowym pliku) z kilkoma polami,
+            c) stworzyc konstruktory, gettery i settery
+            d) nadpisać metodę toString aby zwracała wszystkie elementy ładnie przedstawione :)
+         */
 
-
-    }
-
-    enum LiczbyEnum {
-
-        JEDEN,DWA,TRZY,CZTERY,PIEC,SZESC;
-
-    }
-
-    enum StatusEnum{
-
-        KONTYNUUJEMY,KONIEC;
-
-    }
-
-
-}
+        /* zad.3
+            a) W klasie Main stworzyć 5 różnych studentów, (można za pomocą tego samego objektu)
+            b) dodać studentów do Listy ArrayList,
+            c) przeiterować listę za pomocą pętli forEach wywołując na każdym objekcie metodę toString() (w każdej iteracji);
+         */
